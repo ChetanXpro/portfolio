ee28726d3236b5c314e939ca90348430d827f212
Chetan Baliyan <chetanbaliyan10@gmail.com>
2023-03-05 10:21:24 +0530

    feat: add hero and about section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,19 @@ const Hero = () => {
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develop 3D visual , user <br className='sm:block hidden' /> interface and web application</p>
         </div>
       </div>
-        <ComputersCanvas />
+      <ComputersCanvas />
+      <div className='absolute xs:bottom-10 botton-32 w-full flex justify-center items-center'>
+        <a href="#about">
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div animate={{ y: [0, 24, 0] }} transition={{
+              duration: 1.5,
+              repeat: Infinity,
+              repeatType: 'loop'
+
+            }} className='w-3 h-3 rounded-full bg-secondary mb-1' />
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
--- src/utils/motion.js
+++ src/utils/motion.js
@@ -1,4 +1,4 @@
-export const textVariant = (delay) => {
+export const  textVariant = (delay) => {
     return {
       hidden: {
         y: -50,
